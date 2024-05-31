a9b64ddbb755fc9b723611289c2f27b811866cd9
root <tammam744@gmail.com>
2024-05-31 23:29:42 +0300

    Marketing & Application layout
    1- Added a marketing layout 'non-authenticated user access' -> left auth integrity
    2- Application layout 'For the dashboard / auth-user' -> left auth integrity
    3- Added the nav menu items to the application helper 'To be handled, mapped to display there'
    4- Added the accounts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'accounts/index'
   devise_for :users
   root 'static_pages#root'
-
   post '/webhooks/:source', to: 'webhooks#create'
+
+  resource :dashboard
+  resources :accounts
 end
